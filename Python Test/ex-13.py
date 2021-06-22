ece50ae8087bce386f4bea6a5a666c73774a4441
jet wong <30531175+wongzhe@users.noreply.github.com>
2021-06-22 23:21:32 +0800

    Update ex-13.py

--- Python Test/ex-13.py
+++ Python Test/ex-13.py
@@ -6,3 +6,5 @@ script, first, second, third = argv
 print("Your first variable is:", first)
 print("Your second varizble is:", second)
 print("Your third variable is:", third)
+#1、运行的时候在python 文件下运行，不要在IDLE模式下运行。
+#2、运行命令（python ex13.py first 2nd 3rd ）注意后面要跟（first 2nd 3rd）
