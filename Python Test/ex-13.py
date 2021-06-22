c5c76cd15d48d57b67bac1a38a28df215a3cd250
jet wong <30531175+wongzhe@users.noreply.github.com>
2021-06-22 22:17:20 +0800

    Add files via upload

---
+++ Python Test/ex-13.py
@@ -0,0 +1,8 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, third = argv
+
+
+print("Your first variable is:", first)
+print("Your second varizble is:", second)
+print("Your third variable is:", third)
